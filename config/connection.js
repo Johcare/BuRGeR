9f66048caf1c8eb7415e826b1b459d6884bad3c7
Johcare <johcare2@gmail.com>
2017-09-28 13:19:44 -0400

    test

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Sets up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+// Makes connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Exports connection for our ORM to use.
+module.exports = connection;
